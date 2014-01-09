ad17192c64f62d9edae77d4c53f22231e876fce7
Daniel Eden <dan.eden@me.com>
2014-01-08 16:38:22 -0800

    Whole number pixels only

--- tasks/remfallback.js
+++ tasks/remfallback.js
@@ -63,7 +63,7 @@ module.exports = function(grunt){
 
             // replace 'rem' and anything that comes after it, we'll repair this later
             var unitlessValue = v.replace(/rem.*/, '');
-            var pxValue = preciseRound(unitlessValue * rootSize, 1) + 'px';
+            var pxValue = preciseRound(unitlessValue * rootSize, 0) + 'px';
             var newValue = restValue ? pxValue + restValue : pxValue;
 
             remFound++;
